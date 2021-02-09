5de00f599d18bf712d0030035a139f32ded06e56
Petter Ekedahl <petter.ekedahl@gmail.com>
2021-02-09 14:03:40 +0100

    Uppdatering v10000

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 const express = require('express');
 const app = express();
 const websocket = require('ws');
+
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+    console.log("Server is listening");
+})
+
+const wsServer = new WebSocket.Server({ port: 1337 });
+
+wsServer.on('connection', (websocket) => {
+    websocket.on('message', (message) => {
+        
+    });
+});
